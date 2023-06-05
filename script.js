8060acc937439625481a94d80f8b6a96515b0505
Anna Drewing <anna.drewing@tutanota.com>
2023-06-05 20:49:42 +0200

    mocking server through async/await and a promise

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let input = document.querySelector("#url");
 input.addEventListener("input", validateInput);
 
 // Existence check occurs when the user clicks away
-input.addEventListener("input", throttle(runExistenceCheck, 3000));
+input.addEventListener("input", throttle(runExistenceCheck, 1000));
 
 // Source: https://stackoverflow.com/questions/7082527/jquery-throttling-and-queuing-of-ajax-requests
 function throttle(func, wait) {
@@ -24,42 +24,41 @@ function throttle(func, wait) {
   };
 }
 
+function showHint(hint) {
+  hint.style.display = "block";
+}
+
+function hideAllHints() {
+  const hints = document.querySelectorAll(".hint");
+  [...hints].forEach((hint) => (hint.style.display = "none"));
+}
 /**
  * Checks whether the given URL is of valid format.
  * According to MDN docs: "A single properly-formed absolute URL. This doesn't necessarily mean the URL address exists, but it is at least formatted correctly. In simple terms, this means urlscheme://restofurl"
  */
 function validateInput() {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function () {
-    hideAllHints();
-    let inputIsValid = input.value && input.checkValidity();
-    if (!inputIsValid) {
-      let hint = document.getElementById("hint");
-      showHint(hint);
-    }
-  };
-  let url = input.value;
-  try {
-    xhttp.open("GET", url, true);
-    xhttp.send();
-  } catch (error) {
-    console.error("Ooops, we've got a problem! " + error);
+  hideAllHints();
+  let inputIsValid = input.value && input.checkValidity();
+  if (!inputIsValid) {
+    let urlHint = document.getElementById("url-hint");
+    showHint(urlHint);
   }
 }
 
 /**
  * Checks whether the given URL leads to a file or a folder
  */
-function runExistenceCheck() {
-  var xhttp = new XMLHttpRequest();
+async function runExistenceCheck() {
   let url = input.value;
-  xhttp.onreadystatechange = function () {
-    // Source of the regular expression: https://stackoverflow.com/questions/57960849/regex-to-match-a-url-with-no-file-extension
-    const folderRegex = new RegExp(/(?:\/|^)[^.\/]+$/);
-    let inputIsValid = input.value && input.checkValidity();
-    // Running the existence check only if input is valid
-    if (inputIsValid) {
-      hideAllHints();
+  // Source of the regular expression: https://stackoverflow.com/questions/57960849/regex-to-match-a-url-with-no-file-extension
+  const folderRegex = new RegExp(/(?:\/|^)[^.\/]+$/);
+  let inputIsValid = input.value && input.checkValidity();
+  // Running the existence check only if input is valid
+  if (inputIsValid) {
+    hideAllHints();
+    const serverCallResult = await mockServerResponse(url);
+    // Check if folder or file
+    if (serverCallResult === 200) {
       if (folderRegex.test(input.value)) {
         let folderInfo = document.getElementById("folder");
         showHint(folderInfo);
@@ -67,25 +66,25 @@ function runExistenceCheck() {
         let fileInfo = document.getElementById("file");
         showHint(fileInfo);
       }
+    } else {
+      let serverNope = document.getElementById("server");
+      showHint(serverNope);
     }
-  };
-  try {
-    xhttp.open("GET", url, true);
-    xhttp.send();
-  } catch (error) {
-    console.error("Ooops, we've got a problem! " + error);
   }
 }
 
-function showHint(hint) {
-  hint.style.display = "block";
-}
-
-function hideAllHints() {
-  let folderInfo = document.getElementById("folder");
-  folderInfo.style.display = "none";
-  let fileInfo = document.getElementById("file");
-  fileInfo.style.display = "none";
-  let hint = document.getElementById("hint");
-  hint.style.display = "none";
+function mockServerResponse(url) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      //Randomly resolve or reject
+      const randomNum = Math.floor(Math.random() * 10);
+      if (randomNum < 5) {
+        resolve(200); //Status code for successful server response
+      } else {
+        reject(502); //Status code for "Bad Gateway"
+      }
+    }, 1000);
+  }).catch((err) => {
+    console.log("We got a server error: " + err);
+  });
 }
